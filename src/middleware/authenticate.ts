8b900080ffc0a610dff1bcd82d32c84af244a3e3
blaxsior <heejun2798@gmail.com>
2023-02-19 17:52:55 +0900

    유저 식별을 위한 미들웨어 추가

---
+++ src/middleware/authenticate.ts
@@ -0,0 +1,12 @@
+import { RequestHandler } from "express";
+import { UserEntity } from "../model/user.js";
+
+export const authenticate: RequestHandler = async (req, res, next) => {
+    const uid = req.cookies['uid'];
+    const user = await UserEntity.findById(uid);
+    if(user) {
+        req.user = user;
+    }
+
+    next();
+}
